7b6586bee2e12a3f812b17f07f76966ea1f43519
ravireddy07 <ravikanthreddy8500@gmail.com>
2019-10-09 11:14:14 +0530

    Weights updated

--- model.py
+++ model.py
@@ -16,23 +16,22 @@ class QNetwork(nn.Module):
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
         "*** YOUR CODE HERE ***"
-        
+
         fc1_units = 1024 #256
         fc2_units = 1024 #256
-        
+
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
-        
-  
+
+
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        
+
         x = F.relu(self.fc1(state))
         #x = F.dropout(x, p=0.0, training=self.training)
         x = F.relu(self.fc2(x))
         #x = F.dropout(x, p=0.0, training=self.training)
         x = self.fc3(x)
-        
+
         return x
-        
